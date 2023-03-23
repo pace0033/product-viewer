c46d37f6bb7dcfabc639eafa7b9e70a83ce89e6d
Will Paceley <pace0033@algonquinlive.com>
2023-03-23 10:42:31 -0400

    add initial fetch call and render results

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,27 @@ import './App.css';
 function App() {
   const [products, setProducts] = useState([]);
 
+  useEffect(() => {
+    // Fetch the initial list of products
+    fetch('https://dummyjson.com/products')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setProducts(data.products);
+      })
+      .catch(console.error);
+  }, []);
+
   return (
     <div className="App">
       <h1>Product Viewer</h1>
       <ul className="product-list">
         {products.length > 0 ? (
-          products.map((product) => <li key={product}>{product}</li>)
+          products.map((product) => (
+            <li key={product.id}>
+              {product.brand} - {product.title}
+            </li>
+          ))
         ) : (
           <li>No products found.</li>
         )}
