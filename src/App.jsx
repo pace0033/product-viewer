6efc169e208400d727551e798bbebc5c81cffcb0
Will Paceley <pace0033@algonquinlive.com>
2023-03-23 10:28:11 -0400

    add products state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
   return (
     <div className="App">
-      <h1>Hello Lovely PA Team!</h1>
+      <h1>Product Viewer</h1>
+      <ul className="product-list">
+        {products.length > 0 ? (
+          products.map((product) => <li key={product}>{product}</li>)
+        ) : (
+          <li>No products found.</li>
+        )}
+      </ul>
     </div>
   );
 }
