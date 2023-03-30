52d50f2af19293ed3520e90f23e6c996f2a1b791
Will Paceley <pace0033@algonquinlive.com>
2023-03-30 10:35:52 -0400

    add initial routes to app

---
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -0,0 +1,7 @@
+import './ProductDetails.css';
+
+function ProductDetails() {
+  return <h1 className="product-details">Product Details Will Go Here!</h1>;
+}
+
+export default ProductDetails;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProductDetails from './components/ProductDetails/ProductDetails';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="product" element={<ProductDetails />} />
+      <Route path="*" element={<h2>404. Something went wrong!</h2>} />
+    </Routes>
+  </BrowserRouter>
+);
