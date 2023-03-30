072832f364cbaac6aa36981305e234df10178eee
Will Paceley <pace0033@algonquinlive.com>
2023-03-30 10:47:42 -0400

    add basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import ProductCard from './components/ProductCard/ProductCard';
 import './App.css';
 
--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -2,8 +2,13 @@ import './ProductCard.css';
 
 export default function ProductCard({ product }) {
   return (
-    <li>
-      {product.brand} - {product.title}
+    <li className="product-card">
+      <p className="label">
+        {product.brand} - {product.title}
+      </p>
+      <button onClick={() => console.log('Details button clicked!')}>
+        Details
+      </button>
     </li>
   );
 }
